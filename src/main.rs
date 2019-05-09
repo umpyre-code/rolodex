79af813baf1d57df7f8b63b6e0d6e23744b29425
Brenden Matthews <brenden@diddyinc.com>
2019-05-09 10:34:56 -0400

    Drop MTLS between rolodex <> turnstile for now.
    The TLS features w/ tower are incomplete, so let's punt on this for now.

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,6 @@ extern crate phonenumber;
 extern crate r2d2_redis;
 extern crate regex;
 extern crate rolodex_grpc;
-extern crate tokio_rustls;
 extern crate toml;
 extern crate trust_dns;
 extern crate url;
@@ -35,50 +34,8 @@ mod service;
 use futures::{Future, Stream};
 use r2d2_redis::RedisConnectionManager;
 use rolodex_grpc::proto::server;
-use std::fs::File;
-use std::io::BufReader;
-use std::sync::{Arc, Mutex};
 use tokio::net::TcpListener;
-use tokio_rustls::rustls::internal::pemfile::{certs, rsa_private_keys};
-use tokio_rustls::rustls::{
-    AllowAnyAuthenticatedClient, Certificate, PrivateKey, RootCertStore, ServerConfig,
-};
-use tokio_rustls::TlsAcceptor;
 use tower_hyper::server::{Http, Server};
-
-fn load_certs(path: &str) -> Vec<Certificate> {
-    certs(&mut BufReader::new(
-        File::open(path).expect("Couldn't open file"),
-    ))
-    .expect("Unable to load certs")
-}
-
-fn load_keys(path: &str) -> Vec<PrivateKey> {
-    rsa_private_keys(&mut BufReader::new(
-        File::open(path).expect("Couldn't open file"),
-    ))
-    .expect("Unable to read private keys")
-}
-
-fn get_tls_config() -> TlsAcceptor {
-    // TLS config
-    // load root CA
-    let mut root_cert_store = RootCertStore::empty();
-    load_certs(&config::CONFIG.service.ca_cert_path)
-        .iter()
-        .for_each(|cert| {
-            root_cert_store.add(cert).expect("Unable to load root CA");
-        });
-    let mut tls_config = ServerConfig::new(AllowAnyAuthenticatedClient::new(root_cert_store));
-    // load server certs
-    tls_config
-        .set_single_cert(
-            load_certs(&config::CONFIG.service.tls_cert_path),
-            load_keys(&config::CONFIG.service.tls_key_path).remove(0),
-        )
-        .expect("invalid key or certificate");
-    TlsAcceptor::from(Arc::new(tls_config))
-}
 
 fn get_db_pool(
     database: &config::Database,
@@ -133,35 +90,54 @@ pub fn main() {
         get_redis_pool(&config::CONFIG.redis.writer),
     ));
 
-    let server = Arc::new(Mutex::new(Server::new(new_service)));
+    // Arc<Mutex<>> only needed for TLS version
+    // let server = Arc::new(Mutex::new(Server::new(new_service)));
+    let mut server = Server::new(new_service);
 
     let http = Http::new().http2_only(true).clone();
 
-    let tls_config = get_tls_config();
+    // let tls_config = get_tls_config();
 
     let addr = config::CONFIG.service.bind_to_address.parse().unwrap();
     let bind = TcpListener::bind(&addr).expect("bind");
 
+    // TLS version
+    // let serve = bind
+    //     .incoming()
+    //     .for_each(move |tls_sock| {
+    //         let http = http.clone();
+    //         let server = server.clone();
+    //         let addr = tls_sock.peer_addr().ok();
+    //         if let Err(e) = tls_sock.set_nodelay(true) {
+    //             return Err(e);
+    //         }
+    //         info!("New connection from addr={:?}", addr);
+    //         let done = tls_config
+    //             .accept(tls_sock)
+    //             .and_then(move |sock| {
+    //                 let serve = server.lock().unwrap().serve_with(sock, http.clone());
+    //                 tokio::spawn(serve.map_err(|e| error!("hyper error: {:?}", e)));
+
+    //                 Ok(())
+    //             })
+    //             .map_err(move |err| error!("TLS error: {:?} - {:?}", err, addr));
+    //         tokio::spawn(done);
+
+    //         Ok(())
+    //     })
+    //     .map_err(|e| error!("accept error: {}", e));
+
     let serve = bind
         .incoming()
-        .for_each(move |tls_sock| {
-            let http = http.clone();
-            let server = server.clone();
-            let addr = tls_sock.peer_addr().ok();
-            if let Err(e) = tls_sock.set_nodelay(true) {
+        .for_each(move |sock| {
+            if let Err(e) = sock.set_nodelay(true) {
                 return Err(e);
             }
+            let addr = sock.peer_addr().ok();
             info!("New connection from addr={:?}", addr);
-            let done = tls_config
-                .accept(tls_sock)
-                .and_then(move |sock| {
-                    let serve = server.lock().unwrap().serve_with(sock, http.clone());
-                    tokio::spawn(serve.map_err(|e| error!("hyper error: {:?}", e)));
 
-                    Ok(())
-                })
-                .map_err(move |err| error!("TLS error: {:?} - {:?}", err, addr));
-            tokio::spawn(done);
+            let serve = server.serve_with(sock, http.clone());
+            tokio::spawn(serve.map_err(|e| error!("hyper error: {:?}", e)));
 
             Ok(())
         })
