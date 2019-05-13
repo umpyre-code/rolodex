b8b88b37ac63d06d327215d65cb64d1493c0e2a2
Brenden Matthews <brenden@diddyinc.com>
2019-05-13 15:19:10 -0400

    Add health check binary.

--- lib/build.rs
+++ lib/build.rs
@@ -6,8 +6,4 @@ fn main() {
         .enable_client(true)
         .build(&["proto/rolodex.proto"], &["proto"])
         .unwrap_or_else(|e| panic!("protobuf compilation failed: {}", e));
-    tower_grpc_build::Config::new()
-        .enable_server(true)
-        .build(&["proto/health.proto"], &["proto"])
-        .unwrap_or_else(|e| panic!("protobuf compilation failed: {}", e));
 }
