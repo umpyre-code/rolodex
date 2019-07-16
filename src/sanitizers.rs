c62726dff9fdac0f90928a1ed73ff632be8d88fe
Brenden Matthews <brenden@diddyinc.com>
2019-07-16 14:50:57 -0400

    Better handle sanitization.

--- src/sanitizers.rs
+++ src/sanitizers.rs
@@ -1,5 +1,10 @@
+use regex::Regex;
+
 pub fn handle(value: &str) -> String {
-    let mut value = value.trim().to_string();
+    let value = value.to_string();
+    let value = value.to_lowercase();
+    let re = Regex::new(r"[a-z0-9_.-]").unwrap();
+    let mut value = re.replace_all(&value, "").to_string();
     value.truncate(100);
     value
 }
