7d49228dd910e2d298d68fcba68f3a36fc3d4d03
Brenden Matthews <brenden@diddyinc.com>
2019-10-06 16:13:16 -0400

    Add updated_at trigger migration.

---
+++ migrations/2019-10-06-200233_add_updated_at_trigger/up.sql
@@ -0,0 +1,5 @@
+SELECT diesel_manage_updated_at('clients');
+SELECT diesel_manage_updated_at('client_account_actions');
+SELECT diesel_manage_updated_at('phone_verification_codes');
+SELECT diesel_manage_updated_at('prefs');
+SELECT diesel_manage_updated_at('unique_email_addresses');
